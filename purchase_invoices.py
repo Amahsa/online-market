e1cfc1448886c7d52503ef192cfc42a376f6820d
Amahsa <aflaki47.m@gmail.com>
2021-10-04 13:28:37 +0330

    remove  purchase_invoices.py

--- purchase_invoices.py
+++
@@ -1,7 +0,0 @@
-class Invoice:
-    def __init__(self, market_name, costumer_name, product, date, total_price):
-        self.market_name = market_name
-        self.costumer_name = costumer_name
-        self.product = product
-        self.date = date
-        self.total_price = total_price
